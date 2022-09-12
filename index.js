f7374aec7b4b3ee2bf524443b13e3cf57a9f31d6
codertinie <martin.munyao@moringaschool.com>
2022-09-12 18:23:44 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,13 +1,24 @@
 function isPalindrome(word) {
   // Write your algorithm here
+  let reversed = word.split("").reverse().join('')
+  if (word === reversed){
+    return true
+  }else{
+    return false
+  }
 }
 
 /* 
   Add your pseudocode here
+  if the word(input) is the same as the word in reverse, JS should return true otherwise 
+  it should return false
 */
 
 /*
   Add written explanation of your solution here
+  split the word into an array
+  reverse the array and put it back togeteher
+  compare the initialstring to the reversed string
 */
 
 // You can run `node index.js` to view these console logs
@@ -20,6 +31,11 @@ if (require.main === module) {
 
   console.log("Expecting: false");
   console.log("=>", isPalindrome("robot"));
+
+  console.log("")
+
+  console.log("Expecting: true");
+  console.log("=>", isPalindrome("radar"));
 }
 
 module.exports = isPalindrome;
